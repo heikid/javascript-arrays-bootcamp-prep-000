2aa03bc355b058a978ae1606c2a9ed2749e1839e
Hazelin Ngan <hazelin.ngan@gmail.com>
2017-08-30 01:11:51 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,6 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray (){
+  chocolateBars = ["element", ...chocolateBars];
+  return chocolateBars;
+}
